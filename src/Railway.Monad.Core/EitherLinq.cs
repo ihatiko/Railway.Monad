42726af9f12505a6cdf99469d2a8037f390fa01e
Morozov Vyacheslav <verdark768@yandex.ru>
2020-09-11 00:37:10 +0300

    sample linq

---
+++ src/Railway.Monad.Core/EitherLinq.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Railway.Monad.Core
+{
+    public static class EitherLinq
+    {
+        public static Either<TNewResult, TNewError> Select<TResult, TError, TNewResult, TNewError>(this Either<TResult, TError> either, Func<Either<TResult, TError>, Either<TNewResult, TNewError>> projection)
+        {
+            return projection(either);
+        }
+    }
+}
--- src/Railway.Monad.Sandbox/Program.cs
+++ src/Railway.Monad.Sandbox/Program.cs
@@ -1,31 +1,15 @@
 ﻿using System;
+using Railway.Monad.Core;
 
 namespace Railway.Monad
 {
-    public struct Either<TResult, TError>
-    {
-        internal TResult Result;
-        internal TError Error;
-        
-        public Either(TResult result)
-        {
-            Result = result;
-            Error = default;
-        }
-        public Either(TError error)
-        {
-            Error = error;
-            Result = default;
-        }
-
-        public static implicit operator Either<TResult, TError>(TResult result) => new Either<TResult, TError>(result);
-        public static implicit operator Either<TResult, TError>(TError error) => new Either<TResult, TError>(error);
-    }
     class Program
     {
         static void Main(string[] args)
         {
             Either<int, string> test = (10, "");
+            var a = from x in test.Select(item => item.MapRight(item => 10f))
+                    select x;
         }
     }
 }
